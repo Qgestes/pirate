f7cb9fedda1822e256375f697bb5633ebf550007
Qgestes <Qgestes@macbookequentin.home>
2019-03-09 21:23:26 +0100

    linting

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,59 +1,42 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
 const BoardCellDiv = styled.div`
-padding:40px;
-border:1px solid black;
+  padding: 40px;
+  border: 1px solid black;
 `;
 
-
-const BoardCell = ({i, j}) => { 
-
-	return (
-		<BoardCellDiv> {`(${i} , ${j})`} </BoardCellDiv>
-	)	
-
-}
-
+const BoardCell = ({ i, j }) => {
+  return (
+    <BoardCellDiv>
+      {`(${i} , ${j})`}
+    </BoardCellDiv>
+  );
+};
 
 const BoardRowDiv = styled.div`
-display:flex;
-flex-direction:columns;
+  display: flex;
+  flex-direction: columns;
 `;
 
-const BoardRow = ({i}) => { 
+const BoardRow = ({ i }) => {
+  const columns = [];
 
-	const columns = [];
-	
-	
-	for (let j=0; j<5; j++){
-		columns [j] = <BoardCell i={i} j={j}/>;
-	}
+  for (let j = 0; j < 5; j = j + 1) {
+    columns[j] = <BoardCell i={i} j={j} />;
+  }
 
-	
-	return (
-		<BoardRowDiv>
-			{columns}
-		</BoardRowDiv>
-	)	
+  return <BoardRowDiv>{columns}</BoardRowDiv>;
+};
 
-}
+const Board = props => {
+  const rows = [];
 
-const Board = (props) => { 
+  for (let i = 0; i < 5; i = i + 1) {
+    rows[i] = <BoardRow i={i} />;
+  }
 
-	const rows = [];
-	
-	for (let i=0; i<5; i++){
-
-		rows[i] = <BoardRow i={i}/>;
-	}
-	
-	return (
-		<div>
-			{rows}
-		</div>
-	)	
-
-}
+  return <div>{rows}</div>;
+};
 
 export default Board;
